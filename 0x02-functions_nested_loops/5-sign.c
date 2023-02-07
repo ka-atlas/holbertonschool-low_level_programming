20e4c48f2f1eba1e5b9d3d553a15b4119473ffbc
kush-holberton <kush.aswani@holbertonschool.com>
2023-02-07 11:12:20 -0600

    updated the 5th file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,15 @@
 #include "main.h"
+#include <stdio.h>
 
 int print_sign(int n)
 {
 	if (n>0)
+		printf("+");
 		return (1);
 	else if (n<0)
+		printf("-");
                 return (-1);
 	else
+		printf("0");
 		return (0);
 }
