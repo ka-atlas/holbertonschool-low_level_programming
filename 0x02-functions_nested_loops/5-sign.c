9d177b08fab4ed59f8db6ec9df2f6651b5d76be7
kush-holberton <kush.aswani@holbertonschool.com>
2023-02-07 11:44:22 -0600

    updated the 5th file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,15 +4,15 @@
 int print_sign(int n)
 {
 	if (n>0){
-		printf("+1");
+		_putchar("+");
 		return (1);
 	}
 	else if (n<0){
-		printf("-1");
+		_putchar("-");
                 return (-1);
 	}
 	else{
-		printf("0");
+		_putchar("0");
 		return (0);
 	}
 }
