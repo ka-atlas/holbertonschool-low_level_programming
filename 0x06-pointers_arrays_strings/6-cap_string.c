acca0489e44e14698e68b3bf7115490f647ed678
kush-holberton <kush.aswani@holbertonschool.com>
2023-02-08 12:27:08 -0600

    updated the 6th file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,21 +8,25 @@ char *cap_string(char *str)
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
-
-		if (str[index - 1] == ' ' ||
-		    str[index - 1] == '\t' ||
-		    str[index - 1] == '\n' ||
-		    str[index - 1] == ',' ||
-		    str[index - 1] == ';' ||
-		    str[index - 1] == '.' ||
-		    str[index - 1] == '!' ||
-		    str[index - 1] == '?' ||
-		    str[index - 1] == '"' ||
-		    str[index - 1] == '(' ||
-		    str[index - 1] == ')' ||
-		    str[index - 1] == '{' ||
-		    str[index - 1] == '}' ||
-		    index == 0)
+		if (index>0)
+		{	
+			if (str[index - 1] == ' ' ||
+			    str[index - 1] == '\t' ||
+			    str[index - 1] == '\n' ||
+			    str[index - 1] == ',' ||
+			    str[index - 1] == ';' ||
+			    str[index - 1] == '.' ||
+			    str[index - 1] == '!' ||
+			    str[index - 1] == '?' ||
+			    str[index - 1] == '"' ||
+			    str[index - 1] == '(' ||
+			    str[index - 1] == ')' ||
+			    str[index - 1] == '{' ||
+			    str[index - 1] == '}')
+				str[index] -= 32;
+		}
+		
+		else if (str[index] >= 'a' && str[index] <= 'z')
 			str[index] -= 32;
 
 		index++;
