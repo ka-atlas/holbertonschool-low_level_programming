7ef3e9d4e4ff2f0e7a4a8c6c6a0ca5ec1d4cc2c6
kush-holberton <kush.aswani@holbertonschool.com>
2023-02-08 11:00:56 -0600

    updated the 8th file

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,9 +1,19 @@
 #include "main.h"
+#include <stdio.h>
 
 void print_array(int *a, int n)
 {
-	int i;
+	int index;
 
-	for (i=0; i<n; i++)
-		_putchar(a[i]);
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", a[index]);
+
+		if (index == n - 1)
+			continue;
+
+		printf(", ");
+	}
+
+	printf("\n");
 }
