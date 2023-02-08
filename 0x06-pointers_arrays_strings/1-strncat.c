0d71e32e5247c9376649760f8fbf3c339bfa8295
kush-holberton <kush.aswani@holbertonschool.com>
2023-02-08 11:31:17 -0600

    created the 1st file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[dest_len]!='\0')
+		dest_len++;
+
+	while (src[index]<n && index<n)
+        {
+                dest[(dest_len+index)] = src[index];
+                index++;
+        }
+
+	dest[(dest_len+index)] = '\0';
+
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,2 +1,3 @@
 int _putchar(char c);
 char *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, char *src, int n);
