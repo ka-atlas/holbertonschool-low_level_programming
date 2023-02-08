caddf77a464b24a5534a30256f6ab366fee35371
kush-holberton <kush.aswani@holbertonschool.com>
2023-02-08 13:04:12 -0600

    created the 3rd file

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	int c = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] != ' ')
+		{
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+					c++;
+			}
+		}
+		else
+			return (c);
+
+	}
+	return (c);
+}
