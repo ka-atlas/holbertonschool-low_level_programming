0693500fec730780d1eadc932ed54de5e060f842
kush-holberton <kush.aswani@holbertonschool.com>
2023-02-08 10:08:54 -0600

    created the 5th file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len]!='\0')
+		len++;
+	
+	for (i = len - 1; i >= len / 2; i--)
+	{
+		tmp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tmp;
+	}
+}
