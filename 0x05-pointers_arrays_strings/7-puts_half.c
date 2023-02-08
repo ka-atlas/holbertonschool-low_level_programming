1310e67e8a37dc5c637bbbd3a81577c9a948b0a1
kush-holberton <kush.aswani@holbertonschool.com>
2023-02-08 10:47:35 -0600

    created the 7th file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void puts_half(char *str)
+{
+	int len = 0;
+
+	while (s[len]!='\0')
+		len++;
+
+	if ((len%2)==1)
+		index = (len - 1) / 2
+	else
+		index = len/2
+	
+	for (i = index; i<len; i++)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
