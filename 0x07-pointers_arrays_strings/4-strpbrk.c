a93a060d05b7b55c3807d184adbe9e6ad265eb69
kush-holberton <kush.aswani@holbertonschool.com>
2023-02-08 13:12:49 -0600

    updated the 4th file

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,15 +3,18 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while(*s!='\0')
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (i = 0; accept[i] != '\0'; i++)
 		{
-			if (s[i] == accept[j])
-				return s[i];			
+			if (*s == accept[i])
+
+				return s;			
 		}
+
+		s++;
 	}
 
 	return (NULL);
