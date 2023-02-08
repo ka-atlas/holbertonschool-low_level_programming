6a9a7d693ec456fd24719649eab789b181b3f14e
kush-holberton <kush.aswani@holbertonschool.com>
2023-02-08 10:39:16 -0600

    updated the 6th file

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 void puts2(char *str)
-{	
-	int skip = 0;
+{
+        int len = 0;
 
-	while(*str)
-	{	
-		if (skip%2!=1)
-			_putchar(*str++);
-		skip+=1;
-	}
-	_putchar('\n');
+        while (str[len]!='\0')
+        {
+                if ((len%2)==0)
+                        _putchar(str[len]);
+                len++;
+        }
+
+        _putchar('\n');
 }
